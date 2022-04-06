508527346fc8d61dd2dd72f51e75cbd01e7709a7
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-04-06 14:45:31 +0100

    styles added

---
+++ incr-decr-fonts-and-color-changer/app.js
@@ -0,0 +1,7 @@
+const sizeUp = document.querySelector('.sizeUp')
+const sizeDown = document.querySelector('.sizeDown')
+const colorBtn = document.querySelector('.color')
+const p = document.querySelector('p')
+
+// console.log(sizeUp,sizeDown,colorBtn,p)
+
