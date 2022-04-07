7f7fb055f411c569ba192d37c9f8cd3e59d80095
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-04-07 15:00:28 +0100

    2 functions to check password type

--- password-validator/app.js
+++ password-validator/app.js
@@ -5,19 +5,36 @@ const numbers = /[0-9]/;
 const special = /[!@#$%^&*()?]/;
 const minValue = 10;
 
-console.log(p,input);
+console.log(p, input);
 
 const showMsg = () => {
 	if (
-		input.value.length > minValue && input.value.match(letters) &&
+		input.value.length > minValue &&
+		input.value.match(letters) &&
 		input.value.match(numbers) &&
 		input.value.match(special)
 	) {
 		p.textContent = "You have very strong password 🦾";
 		p.style.color = "lime";
+	} else if (
+		input.value.length > minValue &&
+		input.value.match(letters) &&
+		input.value.match(numbers)
+	) {
+		p.textContent = "You have strong password 🙂";
+		p.style.color = "yellow";
 	} else {
-		p.textContent = "none";
+		p.textContent = "You have weak password 😥";
+		p.style.color = "red";
 	}
 };
 
-input.addEventListener('keyup', showMsg)
+const checkPassword = () => {
+    if(input.value !== ''){
+        showMsg()
+    }else{
+        input.textContent = "You didn't put any password"
+    }
+}
+
+input.addEventListener("keyup", checkPassword);
