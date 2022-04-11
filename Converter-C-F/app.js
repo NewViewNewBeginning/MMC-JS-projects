35e82af3a396b2d3782dea3a9399c8d14aeb7d67
Michal Krawczyk <michalkrawczyk1@gmail.com>
2022-04-11 11:27:59 +0100

    listeners for buttons and functions added

--- Converter-C-F/app.js
+++ Converter-C-F/app.js
@@ -1,8 +1,38 @@
-const converter = document.querySelector('#converter')
-const result = document.querySelector('.result')
-const convBtn = document.querySelector('.conv')
-const resetBtn = document.querySelector('.reset')
-const changeBtn = document.querySelector('.change')
-const one = document.querySelector('.one')
-const two = document.querySelector('.two')
+const converter = document.querySelector("#converter");
+const result = document.querySelector(".result");
+const convBtn = document.querySelector(".conv");
+const resetBtn = document.querySelector(".reset");
+const changeBtn = document.querySelector(".change");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
 
+const change = () => {
+	reset();
+	if (one.textContent === "°C") {
+		one.textContent = "°F";
+		two.textContent = "°C";
+	} else {
+		two.textContent = "°F";
+		one.textContent = "°C";
+	}
+};
+
+const convert = () => {
+    if(result.textContent === 0) return
+	if (one.textContent === "°C") {
+		const fahr = Number(converter.value) * 1.8 + 32;
+		result.textContent = `${fahr.toFixed(2)} °F`;
+	} else {
+		const cel = Number(converter.value - 32) / 1.8;
+		result.textContent = `${cel.toFixed(2)} °C`;
+	}
+};
+
+const reset = () => {
+	result.textContent = "";
+	converter.value = "";
+};
+
+resetBtn.addEventListener("click", reset);
+changeBtn.addEventListener("click", change);
+convBtn.addEventListener("click", convert);
